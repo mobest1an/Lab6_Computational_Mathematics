760de4c030dfc7a7f2f31e13d9c212a17913840b
mobest1an <erikk202@gmail.com>
2022-05-30 00:03:55 +0300

    final beta

--- user_io/plot_drawer.py
+++ user_io/plot_drawer.py
@@ -9,10 +9,12 @@ def draw_plot(dots1, dots2):
 
     for x, y in dots1:
         plt.plot(x, y, 'o', color='orange')
-    plt.plot(x_ax1, y_ax1, "orange", linewidth=1.5)
+    plt.plot(x_ax1, y_ax1, "orange", linewidth=1.5, label="Усовершенствованный метод Эйлера")
 
     for x, y in dots2:
         plt.plot(x, y, 'o', color='red')
-    plt.plot(x_ax2, y_ax2, "red", linewidth=1.5)
+    plt.plot(x_ax2, y_ax2, "red", linewidth=1.5, label="Метод Адамса")
+
+    plt.legend()
 
     plt.show()
