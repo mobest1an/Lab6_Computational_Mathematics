2792a740356d9abcc4162de450ea5817ccba8cd0
erik_101 <erik_password228>
2022-05-30 11:24:47 +0300

    final beta

--- user_io/console_output.py
+++ user_io/console_output.py
@@ -1,9 +1,9 @@
 from typing import Callable
 
 
-def output_dots(dots: list, real_function: Callable[[float, float], float], method_name: str):
+def output_dots(dots: list, real_function: Callable[[float], float], method_name: str):
     print("\n\nРезультаты вычисления методом:", method_name)
-    print("%12s%12s%12s%12s" % ("X", "Y", "Real_Y", "Delta_Y"))
+    print("%16s%16s%16s%16s" % ("X", "Y", "Real_Y", "Delta_Y"))
     for dot in dots:
         real_y = round(real_function(dot[0]), 6)
-        print("%12.6f%12.6f%12.6f%12.6f" % (round(dot[0], 6), round(dot[1], 6), real_y, round(abs(real_y - dot[1]), 6)))
+        print("%16.6f%16.6f%16.6f%16.6f" % (round(dot[0], 6), round(dot[1], 6), real_y, round(abs(real_y - dot[1]), 6)))
